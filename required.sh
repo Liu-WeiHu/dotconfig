b6f033f14f783a299c0eaa9c7c3ea3df929c5454
Liu-WeiHu <6460176@qq.com>
2025-04-12 16:22:59 +0800

    fix: update

--- required.sh
+++ required.sh
@@ -6,3 +6,5 @@ rustup default stable
 paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi ffmpegthumbnailer zoxide chafa xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose
 
 mkdir -p {~/.cache/com.pot-app.desktop,~/Code,~/Works,~/Shared,~/Media/Music,~/Media/Pictures/Screenshots,~/Media/Videos/recording}
+
+paru -Rsn pinta htop pacseek loupe imv
