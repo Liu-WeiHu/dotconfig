5ae7eed05a7ad389bae04d773ebb050820e97923
Liu-WeiHu <6460176@qq.com>
2025-04-24 11:35:37 +0800

    fix: update

--- required.sh
+++ required.sh
@@ -3,7 +3,7 @@
 paru -S go rustup
 rustup default stable
 
-paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi ffmpegthumbnailer zoxide chafa xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays
+paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi ffmpegthumbnailer zoxide chafa xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays thunar
 
 mkdir -p {~/.cache/com.pot-app.desktop,~/Code,~/Works,~/Shared,~/Media/Music,~/Media/Pictures/Screenshots,~/Media/Videos/recording}
 
@@ -11,4 +11,4 @@ paru -Rsn pinta htop pacseek loupe imv
 
 # 卸载nautilus
 paru -Rsn nautilus-open-any-terminal nautilus-python nautilus libnautilus-extension
-paru -S poppler-glib libpanel
+# paru -S poppler-glib libpanel
