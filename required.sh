1884c1979fa8af973bf8e8eb04e3d2c910cc0ef6
Liu-WeiHu <6460176@qq.com>
2025-04-28 19:36:47 +0800

    fix: update

--- required.sh
+++ required.sh
@@ -3,7 +3,7 @@
 paru -S go rustup
 rustup default stable
 
-paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi ffmpegthumbnailer zoxide chafa xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays thunar mousepad
+paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi ffmpegthumbnailer zoxide chafa xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays thunar mousepad moreutils
 
 mkdir -p {~/.cache/com.pot-app.desktop,~/Code,~/Works,~/Shared,~/Media/Music,~/Media/Pictures/Screenshots,~/Media/Videos/recording}
 
