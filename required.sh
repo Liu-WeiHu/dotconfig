4e4a704a3ce8a1525989183d63a2feca4a2d7edd
Liu-WeiHu <6460176@qq.com>
2025-05-11 13:29:28 +0800

    fix: pudate

--- required.sh
+++ required.sh
@@ -3,8 +3,12 @@
 paru -S go rustup
 rustup default stable
 
-paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi ffmpegthumbnailer zoxide chafa xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays thunar mousepad moreutils
+paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi 7zip ffmpegthumbnailer zoxide chafa \
+    xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty \
+    npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays thunar mousepad moreutils \
+    thunar-archive-plugin ark unarchiver
 
 mkdir -p {~/.cache/com.pot-app.desktop,~/Code,~/Works,~/Shared,~/Media/Music,~/Media/Pictures/Screenshots,~/Media/Videos/recording}
 
-paru -Rsn pinta htop pacseek loupe imv nautilus-open-any-terminal nautilus-python nautilus libnautilus-extension gnome-calculator gnome-text-editor
+paru -Rsn pinta htop pacseek loupe imv nautilus-open-any-terminal nautilus-python nautilus libnautilus-extension \
+    gnome-calculator gnome-text-editor
