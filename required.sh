3160b1ad20099f78cb3942bf1df21151b76967dd
Liu-WeiHu <6460176@qq.com>
2025-05-21 16:18:29 +0800

    fix: update

--- required.sh
+++ required.sh
@@ -4,7 +4,7 @@ paru -S go rustup
 rustup default stable
 
 paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi 7zip ffmpegthumbnailer zoxide chafa \
-    xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec mpv libreoffice-still libreoffice-still-zh-cn code satty \
+    xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec-git mpv libreoffice-still libreoffice-still-zh-cn code satty \
     npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays thunar mousepad moreutils \
     thunar-archive-plugin ark unarchiver
 
