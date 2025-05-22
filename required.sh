f750cb6eb5f8e4c1bea23483c35e6c95503ae7e0
Liu-WeiHu <6460176@qq.com>
2025-05-22 10:40:27 +0800

    fix:update

--- required.sh
+++ required.sh
@@ -4,7 +4,7 @@ paru -S go rustup
 rustup default stable
 
 paru -S xdg-desktop-portal-gtk less btop fcitx5-breeze pot-translation yazi 7zip ffmpegthumbnailer zoxide chafa \
-    xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec-git mpv libreoffice-still libreoffice-still-zh-cn code satty \
+    xdg-user-dirs xorg-xrdb atuin nvtop wl-screenrec-git mpv libreoffice-still libreoffice-still-zh-cn satty \
     npm lazygit fzf ripgrep fd hyprpolkitagent unzip podman podman-compose nwg-displays ark unarchiver
 
 mkdir -p {~/.cache/com.pot-app.desktop,~/Code,~/Works,~/Shared,~/Media/Music,~/Media/Pictures/Screenshots,~/Media/Videos/recording}
