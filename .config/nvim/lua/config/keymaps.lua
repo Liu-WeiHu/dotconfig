688b1905678d8f4b06187ecabd9bc1dc45daf265
Liu-Weihu <6460176@qq.com>
2025-06-16 02:24:15 +0800

    fix: update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,16 +3,10 @@
 -- Add any additional keymaps here
 
 -- 使用黑洞寄存器配置 s, S, c, C 命令
-vim.keymap.set("n", "d", '"_d', { noremap = true, silent = true })
-vim.keymap.set("n", "D", '"_D', { noremap = true, silent = true })
-vim.keymap.set("n", "c", '"_c', { noremap = true, silent = true })
-vim.keymap.set("n", "C", '"_C', { noremap = true, silent = true })
-
--- 处理可视模式下的情况
-vim.keymap.set("v", "d", '"_d', { noremap = true, silent = true })
-vim.keymap.set("v", "D", '"_D', { noremap = true, silent = true })
-vim.keymap.set("v", "c", '"_c', { noremap = true, silent = true })
-vim.keymap.set("v", "C", '"_C', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "C", '"_C', { noremap = true, silent = true })
 
 -- 映射 插入模式下上下左右移动光标
 vim.keymap.set("i", "<A-h>", "<Left>", { noremap = true, silent = true })
