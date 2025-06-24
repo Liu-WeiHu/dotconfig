d5f03537216b61f3e7bfa0b238aae8b29b0cd379
Liu-WeiHu <6460176@qq.com>
2025-06-24 17:13:25 +0800

    fix: update

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,10 +1,16 @@
 return {
   "mikavilpas/yazi.nvim",
   event = "VeryLazy",
+  dependencies = {
+    -- check the installation instructions at
+    -- https://github.com/folke/snacks.nvim
+    "folke/snacks.nvim",
+  },
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
       "<leader>n",
+      mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
@@ -15,14 +21,12 @@ return {
       desc = "Open the file manager in nvim's working directory",
     },
     {
-      -- NOTE: this requires a version of yazi that includes
-      -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
       "<c-up>",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
   },
-  ---@type YaziConfig
+  ---@type YaziConfig | {}
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
@@ -30,6 +34,12 @@ return {
       show_help = "<f1>",
     },
   },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    -- vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
 
 -- <f1>: show the help menu
